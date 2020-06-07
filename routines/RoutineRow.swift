5bbf5a6b258169065cef16b2cdc5328496edf7fc
Vladimir Pasquier <vpasquier@nuxeo.com>
2020-06-07 19:51:39 -0400

    add buttons make work coredata

--- routines/RoutineRow.swift
+++ routines/RoutineRow.swift
@@ -4,14 +4,14 @@ struct RoutineRow: View {
     var routine: Routine
     
     var body: some View {
-        ProgressBar(name: routine.name!, count: String(routine.count), done: String(routine.done)).frame(height: 70)
+        ProgressBar(name: routine.name!, count: routine.count, done: routine.done).frame(height: 70)
     }
 }
 
 struct ProgressBar: View {
     var name: String
-    var count: String
-    var done: String
+    var count: Int64
+    @State var done: Int64
     
     var body: some View {
         GeometryReader { geometry in
@@ -19,19 +19,34 @@ struct ProgressBar: View {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
                     .opacity(0.3)
                     .foregroundColor(Color(UIColor.systemTeal))
-                Rectangle().frame(width: min(CGFloat(Double(self.done)!)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                Rectangle().frame(width: min(CGFloat(self.done/self.count)*geometry.size.width, geometry.size.width), height: geometry.size.height)
                     .foregroundColor(Color(UIColor.systemBlue))
                     .animation(.linear)
                 HStack {
                     Spacer()
-                    VStack {
-                        Text("\(self.name)").font(.system(.title, design: .rounded))
-                        Text("2/4").font(.system(.caption, design: .rounded))
+                    Button(action: {
+                        if(self.done == 0){
+                            return;
+                        }
+                        self.done -= 1
+                    }) {
+                        Text("-").font(.system(.title, design: .rounded)).padding()
                     }
                     Spacer()
-                    Text("+").font(.system(.title, design: .rounded))
+                    VStack {
+                        Text("\(self.name)").font(.system(.title, design: .rounded))
+                        Text("\(self.done)/\(self.count)").font(.system(.caption, design: .rounded))
+                    }
                     Spacer()
-                    Text("-").font(.system(.title, design: .rounded))
+                    Button(action: {
+                        if(self.done == self.count) {
+                            return;
+                        }
+                        self.done += 1
+                    }) {
+                        Text("+").font(.system(.title, design: .rounded)).padding()
+                    }
+                    Spacer()
                 }
             }.cornerRadius(25.0)
         }
